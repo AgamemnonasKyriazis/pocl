67ff3282242212fd8f6b60b0bf4e2039170c8b77
AgamemnonasKyriazis <agamemnonaskyriazis@gmail.com>
2025-09-29 18:22:30 +0300

    sync

--- lib/CL/devices/cgra/cgra_mem.c
+++ lib/CL/devices/cgra/cgra_mem.c
@@ -2,6 +2,27 @@
 
 int usr_fd;
 
+uint32_t wbuf[] = {
+    0x00000000,
+    0x11111111,
+    0x22222222,
+    0x33333333,
+    0x44444444,
+    0x55555555,
+    0x66666666,
+    0x77777777,
+    0x88888888,
+    0x99999999,
+    0xaaaaaaaa,
+    0xbbbbbbbb,
+    0xcccccccc,
+    0xdddddddd,
+    0xeeeeeeee,
+    0xffffffff,
+};
+
+uint32_t rbuf[sizeof(wbuf)] = {0};
+
 int cgra_write_to_device (
     void *data, const void *__restrict__ host_ptr,
     pocl_mem_identifier *dst_mem_id, cl_mem dst_buf,
@@ -27,7 +48,7 @@ int cgra_write_to_device (
     return -1;
   }
 
-  int wlen = pwrite(usr_fd, (uint32_t*)host_ptr, size, DEVICE_MEM_BASE_ADDR);
+  int wlen = pwrite(usr_fd, (uint32_t*)wbuf, size, DEVICE_MEM_BASE_ADDR);
   if (wlen <= 0 && size != 0)
   {
     perror("Write Error");
@@ -60,7 +81,7 @@ int cgra_read_from_device (
     return -1;
   }
 
-  int rlen = pread(usr_fd, (uint32_t*)host_ptr, size, DEVICE_MEM_BASE_ADDR);
+  int rlen = pread(usr_fd, (uint32_t*)rbuf, size, DEVICE_MEM_BASE_ADDR);
   if (rlen <= 0 && size != 0)
   {
     perror("Read Error");
@@ -71,7 +92,7 @@ int cgra_read_from_device (
 
   printf("Read Sanity Check\n");
   for (int i = 0; i < size/(sizeof(uint32_t)); i+=1)
-    printf("0x%lx\n", ((uint32_t*)host_ptr)[i]);
+    printf("0x%lx\n", ((uint32_t*)rbuf)[i]);
   
   return size;
 }
